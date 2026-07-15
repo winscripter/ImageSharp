1ac61405358cc85f7571fee827860c6fccdf04ee
winscripter <142818255+winscripter@users.noreply.github.com>
2026-07-15 14:44:57 +0400

    Implement AC strategy
    Implementation of ac_strategy.h and ac_strategy.c

---
+++ src/ImageSharp/Formats/Jxl/Ac/JxlAcStrategyImage.cs
@@ -0,0 +1,114 @@
+// Copyright (c) Six Labors.
+// Licensed under the Six Labors Split License.
+
+using System.Diagnostics;
+
+namespace SixLabors.ImageSharp.Formats.Jxl.Ac;
+
+internal sealed class JxlAcStrategyImage
+{
+    private const byte Invalid = byte.MaxValue; // 255
+
+    private JxlImageB? layers;
+    private readonly Memory<byte> row;
+    private readonly int stride;
+
+    public JxlMemoryManager MemoryManager => this.layers.MemoryManager;
+
+    public int XSize => this.layers.XSize;
+
+    public int YSize => this.layers.YSize;
+
+    public int PixelsPerRow => this.layers.PixelsPerRow;
+
+    public JxlAcStrategyRow GetRow(int y, int xPrefix = 0)
+    {
+        ReadOnlyMemory<byte> layerRow = this.layers.GetRow(y);
+        ReadOnlyMemory<byte> row = layerRow[xPrefix..];
+
+        return new JxlAcStrategyRow(row);
+    }
+
+    public static JxlAcStrategyImage Create(JxlMemoryManager memoryManager, int xSize, int ySize)
+    {
+        JxlAcStrategyImage image = new()
+        {
+            layers = JxlImageB.Create(memoryManager, xSize, ySize)
+        };
+
+        image.row = image.layers.GetRow(0);
+        image.stride = image.layers.PixelsPerRow;
+
+        return image;
+    }
+
+    public int CountBlocks(JxlAcStrategyType type)
+    {
+        int value = 0;
+        int compare = ((int)type << 1) | 1;
+
+        for (int y = 0; y < this.layers.YSize; y++)
+        {
+            ReadOnlySpan<byte> row = this.layers.GetRowSpan(y);
+
+            for (int x = 0; x < this.layers.XSize; x++)
+            {
+                if (row[x] == compare)
+                {
+                    value++;
+                }
+            }
+        }
+
+        return value;
+    }
+
+    public JxlAcStrategyRow GetRow(in Rectangle rect, int y) => this.GetRow(rect.Y + y, rect.X);
+
+    public bool IsValid(int x, int y) => this.row.Span[(y * this.stride) + x] != Invalid;
+
+    public bool SetNoBoundsChecks(int x, int y, JxlAcStrategyType type, bool check = true)
+    {
+        JxlAcStrategy strategy = new(type);
+        Span<byte> rowSpan = this.row.Span;
+        int rawType = (int)type;
+        int rawTypeTimes2 = rawType << 1;
+
+        for (int iy = 0; iy < strategy.CoveredBlocksX; iy++)
+        {
+            for (int ix = 0; ix < strategy.CoveredBlocksX; ix++)
+            {
+                int pos = ((y + iy) * this.stride) + x + ix;
+
+                if (check && rowSpan[pos] != Invalid)
+                {
+                    Debug.Fail("Invalid AC strategy. Blocks overlap.");
+
+                    return false;
+                }
+
+                rowSpan[pos] = (byte)(rawTypeTimes2 | ((iy | ix) == 0 ? 1 : 0));
+            }
+        }
+
+        return true;
+    }
+
+    public bool Set(int x, int y, JxlAcStrategyType type)
+    {
+#if DEBUG
+        JxlAcStrategy strategy = new(type);
+
+        Debug.Assert(y + strategy.CoveredBlocksY <= this.layers.YSize, "Invalid range");
+        Debug.Assert(x + strategy.CoveredBlocksX <= this.layers.XSize, "Invalid range");
+#endif
+
+        return this.SetNoBoundsChecks(x, y, type, check: false);
+    }
+
+    public void FillDct8(in Rectangle rect) => this.FillPlane(((int)JxlAcStrategyType.DCT << 1) | 1, this.layers, in rect);
+
+    public void FillDct8() => this.FillDct8(in this.layers.GetRectangle());
+
+    public void FillInvalid() => this.FillImage(Invalid, this.layers);
+}
---
+++ src/ImageSharp/Formats/Jxl/Ac/JxlAcStrategyRow.cs
@@ -0,0 +1,31 @@
+// Copyright (c) Six Labors.
+// Licensed under the Six Labors Split License.
+
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace SixLabors.ImageSharp.Formats.Jxl.Ac;
+
+internal sealed class JxlAcStrategyRow
+{
+    private readonly ReadOnlyMemory<byte> row;
+
+    public JxlAcStrategyRow(ReadOnlyMemory<byte> row) => this.row = row;
+
+    public JxlAcStrategy this[int x]
+    {
+        get
+        {
+            ReadOnlySpan<byte> span = this.row.Span;
+
+            Debug.Assert(x * 8 < span.Length, "Too many bytes of memory were requested");
+
+            ref byte first = ref MemoryMarshal.GetReference(span);
+            JxlAcStrategyType strategy = (JxlAcStrategyType)(Unsafe.Add(ref Unsafe.As<byte, int>(ref first), x) >> 1);
+            bool isFirst = Unsafe.Add(ref first, x) != 0;
+
+            return new JxlAcStrategy(strategy, isFirst);
+        }
+    }
+}
