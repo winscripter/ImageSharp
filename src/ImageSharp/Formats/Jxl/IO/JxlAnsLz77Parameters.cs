3ed56c954f903d216793cd8f28df4fc168cd3fc4
winscripter <142818255+winscripter@users.noreply.github.com>
2026-07-16 23:25:31 +0400

    Add ANS hybrid uint configuration & LZ77 parameters model

---
+++ src/ImageSharp/Formats/Jxl/IO/JxlAnsLz77Parameters.cs
@@ -0,0 +1,21 @@
+// Copyright (c) Six Labors.
+// Licensed under the Six Labors Split License.
+
+using SixLabors.ImageSharp.Formats.Jxl.Fields;
+
+namespace SixLabors.ImageSharp.Formats.Jxl.IO;
+
+internal sealed class JxlAnsLz77Parameters : IJxlFields
+{
+    public bool Enabled { get; set; }
+
+    public uint MinimumSymbol { get; set; }
+
+    public uint MinimumLength { get; set; }
+
+    public JxlAnsHybridUIntConfiguration LengthUintConfig { get; set; } = new(0, 0, 0);
+
+    public int NonserializedDistanceContext { get; set; }
+
+    public bool Visit(JxlVisitor visitor) => throw new NotImplementedException();
+}
